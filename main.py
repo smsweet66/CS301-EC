54debc6f5a0098a6812cf412db6bd884e31300ba
Sean Sweet <sweet.sean42@gmail.com>
2021-11-30 17:35:08 -0600

    added more stuff, fixed printing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from pymongo import MongoClient
+from bson.json_util import dumps
 
 app = Flask(__name__)
 db = MongoClient('localhost', 27017).db
@@ -32,18 +33,29 @@ def homework3():
 
 	return output
 
+
 @app.route('/HW4')
 def homework4():
-	output = ''
-	for document in db.CS301.find({'founded_year': {'$gt': 2000}, 'number_of_employees': {'$gte': 5000}},
-	{"_id":0,"name":1,"founded_year":1,"number_of_employees":1,"total_money_raised":1}):
-		output += str(document) + '<br>'
+	cursor = db.CS301.find({'founded_year': {'$gt': 2000}, 'number_of_employees': {'$gte': 5000}},
+	{"_id":0,"name":1,"founded_year":1,"number_of_employees":1,"total_money_raised":1})
 
+	output = dumps(cursor, indent=1)
+	output = output.replace('\n', '<br>')
 	return output
 
 
-
-#already uses loops, so the same as 4
+# already uses loops, so the same as 4
 @app.route('/HW5')
 def homework5():
-	return homework4
+	return homework4()
+
+
+@app.route('/HW11')
+def homework11():
+	output = ''
+	for document in db.CS301.aggregate([{'$match': {'founded_year': 1800, 'products.name': {'$exists': 'true'}}},
+	{'$project': {'_id':0,'name':1,'homepage_url':1, 'number_of_employees':1,"products.name":1}}]):
+		output += dumps(document) + '\n'
+
+	output = output.replace('\n', '<br>')
+	return output
