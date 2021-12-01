e628aa29349afdd3a29b72fc90dd386780b9238b
Sean Sweet <smsweet@crimson.ua.edu>
2021-11-30 20:44:22 -0600

    added more homework questions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pymongo
 from flask import Flask
 from pymongo import MongoClient
 
@@ -18,7 +19,7 @@ def homework1():
 @app.route('/HW2')
 def homework2():
 	output = ''
-	for document in db.CS301.find({"milestones.stoneable.name": "Zoho"}, {"_id":0,"twitter_username":1,"category_code":1}):
+	for document in db.CS301.find({'milestones.stoneable.name': 'Zoho'}, {'_id':0,'twitter_username':1,'category_code':1}):
 		output += str(document) + '<br>'
 
 	return output
@@ -27,23 +28,62 @@ def homework2():
 @app.route('/HW3')
 def homework3():
 	output = ''
-	for document in db.CS301.find({}, {"_id":0,"twitter_username":1}):
+	for document in db.CS301.find({}, {'_id':0,'twitter_username':1}):
 		output += str(document) + '<br>'
 
 	return output
+
 
 @app.route('/HW4')
 def homework4():
 	output = ''
 	for document in db.CS301.find({'founded_year': {'$gt': 2000}, 'number_of_employees': {'$gte': 5000}},
-	{"_id":0,"name":1,"founded_year":1,"number_of_employees":1,"total_money_raised":1}):
+	{'_id':0,'name':1,'founded_year':1,'number_of_employees':1,'total_money_raised':1}):
 		output += str(document) + '<br>'
 
 	return output
 
 
-
-#already uses loops, so the same as 4
+# already uses loops, so the same as 4
 @app.route('/HW5')
 def homework5():
 	return homework4
+
+
+@app.route('/HW6')
+def homework6():
+	output = ''
+	for document in db.CS301.find({'founded_month': {'$exists':'false'}}, {'_id':1}):
+		output += str(document) + '<br>'
+
+	return output
+
+
+@app.route('/HW7')
+def homework7():
+	return str(db.CS301.count_documents({'funding_rounds.raised_amount': {'$gt':5000000}}))
+
+
+@app.route('/HW8')
+def homework8():
+	return str(db.CS301.find({'funding_rounds.raised_amount': {'$gt':5000000}}).count())
+
+
+@app.route('/HW9')
+def homework9():
+	output = ''
+	for document in db.CS301.find({'$or': [{'founded_year': {'$gt':2012}}, {'founded_year': {'$lt':1805}}]},
+	{'_id':0,'name':1,'founded_year':1}).sort([('founded_year', pymongo.DESCENDING), ('name', pymongo.ASCENDING)]):
+		output += str(document) + '<br>'
+
+	return output
+
+
+@app.route('/HW10')
+def homework10():
+	output = ''
+	for document in db.CS301.find({'founded_year': 1800, 'products.name': {'$exists': 'true'}},
+	{'_id':0,'name':1,'homepage_url':1,'number_of_employees':1,'products.name':1}):
+		output += str(document) + '<br>'
+
+	return output
