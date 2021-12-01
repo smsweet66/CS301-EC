1a72276b7ea2585852bbca20ea5a0df2ed0833bf
Sean Sweet <smsweet@crimson.ua.edu>
2021-11-30 20:57:29 -0600

    changed how everything is printed

--- main.py
+++ main.py
@@ -18,19 +18,17 @@ def homework1():
 
 @app.route('/HW2')
 def homework2():
-	output = ''
-	for document in db.CS301.find({'milestones.stoneable.name': 'Zoho'}, {'_id':0,'twitter_username':1,'category_code':1}):
-		output += str(document) + '<br>'
-
+	cursor = db.CS301.find({'milestones.stoneable.name': 'Zoho'}, {'_id':0,'twitter_username':1,'category_code':1})
+	output = dumps(cursor, indent=1)
+	output = output.replace('\n', '<br>')
 	return output
 
 
 @app.route('/HW3')
 def homework3():
-	output = ''
-	for document in db.CS301.find({}, {'_id':0,'twitter_username':1}):
-		output += str(document) + '<br>'
-
+	cursor = db.CS301.find({}, {'_id':0,'twitter_username':1})
+	output = dumps(cursor, indent=1)
+	output = output.replace('\n', '<br>')
 	return output
 
 
@@ -47,15 +45,20 @@ def homework4():
 # already uses loops, so the same as 4
 @app.route('/HW5')
 def homework5():
-	return homework4
+	cursor = ''
+	for document in db.CS301.find({'founded_year': {'$gt': 2000}, 'number_of_employees': {'$gte': 5000}},
+	{"_id":0,"name":1,"founded_year":1,"number_of_employees":1,"total_money_raised":1}):
+		cursor += dumps(document, indent=1) + '\n'
+
+	cursor = cursor.replace('\n', '<br>')
+	return cursor
 
 
 @app.route('/HW6')
 def homework6():
-	output = ''
-	for document in db.CS301.find({'founded_month': {'$exists':'false'}}, {'_id':1}):
-		output += str(document) + '<br>'
-
+	cursor = db.CS301.find({'founded_month': {'$exists':'false'}}, {'_id':1})
+	output = dumps(cursor, indent=1)
+	output = output.replace('\n', '<br>')
 	return output
 
 
@@ -71,30 +74,29 @@ def homework8():
 
 @app.route('/HW9')
 def homework9():
-	output = ''
-	for document in db.CS301.find({'$or': [{'founded_year': {'$gt':2012}}, {'founded_year': {'$lt':1805}}]},
-	{'_id':0,'name':1,'founded_year':1}).sort([('founded_year', pymongo.DESCENDING), ('name', pymongo.ASCENDING)]):
-		output += str(document) + '<br>'
+	cursor = db.CS301.find({'$or': [{'founded_year': {'$gt':2012}}, {'founded_year': {'$lt':1805}}]},
+	{'_id':0,'name':1,'founded_year':1}).sort([('founded_year', pymongo.DESCENDING), ('name', pymongo.ASCENDING)])
 
+	output = dumps(cursor, indent=1)
+	output = output.replace('\n', '<br>')
 	return output
 
 
 @app.route('/HW10')
 def homework10():
-	output = ''
-	for document in db.CS301.find({'founded_year': 1800, 'products.name': {'$exists': 'true'}},
-	{'_id':0,'name':1,'homepage_url':1,'number_of_employees':1,'products.name':1}):
-		output += str(document) + '<br>'
+	cursor = db.CS301.find({'founded_year': 1800, 'products.name': {'$exists': 'true'}},
+	{'_id':0,'name':1,'homepage_url':1,'number_of_employees':1,'products.name':1})
 
+	output = dumps(cursor, indent=1)
+	output = output.replace('\n', '<br>')
 	return output
 
 
 @app.route('/HW11')
 def homework11():
-	output = ''
-	for document in db.CS301.aggregate([{'$match': {'founded_year': 1800, 'products.name': {'$exists': 'true'}}},
-	{'$project': {'_id':0,'name':1,'homepage_url':1, 'number_of_employees':1,"products.name":1}}]):
-		output += dumps(document) + '\n'
+	cursor = db.CS301.aggregate([{'$match': {'founded_year': 1800, 'products.name': {'$exists': 'true'}}},
+	{'$project': {'_id':0,'name':1,'homepage_url':1, 'number_of_employees':1,"products.name":1}}])
 
+	output = dumps(cursor, indent=1)
 	output = output.replace('\n', '<br>')
 	return output
